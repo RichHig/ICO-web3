c441b148f1d07455fb0be67b9f86ef94e9d43ae3
Richard Higenbottam <rich_higenbottam@hotmail.com>
2023-01-07 20:07:14 +0000

    git commit -m "first commit"

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x60EeA6F06B504e8ac2b600746137E1fbB5BDf6eB";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
